cae65a759e352a2234edb68c02939b3cce455f19
JagdishChavan81 <jagdish.chavan081@gmail.com>
2022-11-25 18:19:24 +0530

    create views and urls

--- countriesAPI/countries/views.py
+++ countriesAPI/countries/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.parser import JSONParser
+from rest_framework import status
+
+
 
 # Create your views here.
